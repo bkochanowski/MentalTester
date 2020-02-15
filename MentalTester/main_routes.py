3f478102a2a00fae9b90926797e39d031260790c
Bartek <kochan88@gmail.com>
2020-02-15 11:25:42 +0100

    routes restrictions added

--- MentalTester/main_routes.py
+++ MentalTester/main_routes.py
@@ -1,5 +1,4 @@
 from flask import Blueprint, render_template
-from flask_login import login_required, current_user
 
 main = Blueprint('main', __name__)
 
@@ -10,6 +9,5 @@ def index():
 
 
 @main.route('/profile')
-@login_required
 def profile():
-    return render_template('profile.html', name=current_user.username)
+    return render_template('profile.html')
